80a4755315ce4a4142e405ac22e2955f860a3b33
Tadei-Mykola <tmukola2006@gmail.com>
2024-01-24 14:29:57 +0200

    add interface

---
+++ src/app/bank-list/bank-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bank-list',
+  templateUrl: './bank-list.component.html',
+  styleUrls: ['./bank-list.component.scss']
+})
+export class BankListComponent {
+
+}
